bb80153ca23e444ecdf2ed991265c402488a70be
Mehmet ERÖZKAN <92365223+mehmeterozkan91@users.noreply.github.com>
2023-12-30 23:37:10 +0300

    operators

---
+++ Numerical-Operators/src/operators/additive/Subtraction.java
@@ -0,0 +1,20 @@
+package operators.additive;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int number1 = 8;
+        int number2 = 7;
+        int result1 = number1 - number2;
+        System.out.println("number1 - number2 = " + result1); // number1 - number2 = 1
+
+        long longNumber1 = 4L;
+        long longNumber2 = 12L;
+        long result2 = longNumber1 - longNumber2;
+        System.out.println("longNumber1 - longNumber2 = " + result2);
+
+        float floatNumber1 = 5f;
+        float floatNumber2 = 3f;
+        float result3 = floatNumber1 - floatNumber2;
+        System.out.println("floatNumber1 - floatNumber2 =" + result3);
+    }
+}
---
+++ Numerical-Operators/src/operators/increment/Prefix.java
@@ -0,0 +1,17 @@
+package operators.increment;
+
+public class Prefix {
+    public static void main(String[] args) {
+        int number1 = 8;
+        int number2 = ++number1;
+        System.out.println("number2 = " + number2); // number2 = 9
+
+        long longNumber1 = 4L;
+        long longNumber2 = ++longNumber1;
+        System.out.println("longNumber2 = " + longNumber2);
+
+        float floatNumber1 = 5f;
+        float floatNumber2 = ++floatNumber1;
+        System.out.println("floatNumber2 = " + floatNumber2);
+    }
+}
