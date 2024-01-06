af083333e9896345d014a16b5cd31028ff0fbff5
Mehmet ERÖZKAN <92365223+mehmeterozkan91@users.noreply.github.com>
2024-01-07 00:13:05 +0300

    operators

---
+++ Numerical-Operators/src/operators/shift/Signed.java
@@ -0,0 +1,15 @@
+package operators.shift;
+
+public class Signed {
+    public static void main(String[] args) {
+// << (Sol Kaydırma Operatörü - Left Shift Operator)
+        int number1 = 5;  // 0000 0101 (5)
+        int number2 = number1 << 2; // 0001 0100 (20)
+        System.out.println("number2 = " + number2); // number2 = 20
+
+// >> (Sağ Kaydırma Operatörü - Right Shift Operator)
+        int number3 = -16;  // 1111 0000 (-16)
+        int number4 = number3 >> 2; // 1111 1100 (-4)
+        System.out.println("number4 = " + number4); // number4 = -4
+    }
+}
---
+++ Numerical-Operators/src/operators/shift/Unsigned.java
@@ -0,0 +1,10 @@
+package operators.shift;
+
+public class Unsigned {
+    public static void main(String[] args) {
+// >>> (Unsigned Right Shift Operator)
+        int number5 = -16;  // // 1111 1111 1111 1111 1111 1111 1111 0000
+        int number6 = number5 >>> 2; // 0011 1111 1111 1111 1111 1111 1111 1100 (1073741820)
+        System.out.println("number6 = " + number6); // number6 = 1073741820
+    }
+}
